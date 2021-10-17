581ba8d5aeb5b4e18e4dad3470af46465f9cbc14
jcu011 <jcu011@naver.com>
2021-10-18 06:26:30 +0900

    mod User entity & Post entity

--- src/main/java/com/devcourse/board/domain/post/Post.java
+++ src/main/java/com/devcourse/board/domain/post/Post.java
@@ -5,21 +5,17 @@ import com.devcourse.board.domain.post.vo.Content;
 import com.devcourse.board.domain.post.vo.Title;
 import com.devcourse.board.domain.user.User;
 import java.util.Objects;
-import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Builder
